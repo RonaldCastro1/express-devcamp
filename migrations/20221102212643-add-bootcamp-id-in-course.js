19c9dca7155f22946df07391ee4049e5fe92c127
Usuario <Email Usuario>
2022-11-02 16:36:55 -0500

    migrate

---
+++ migrations/20221102212643-add-bootcamp-id-in-course.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+      //Add colum: argumentos
+      await queryInterface.addColumn('courses', 'bootcamp_id', {type:Sequelize.INTEGER,references:{model:'bootcamps', key:'id'},onUpdate: 'CASCADE', onDelete: 'SET NULL'})
+    },
+  
+    async down (queryInterface, Sequelize) {
+      await queryInterface.removeColumn('courses', 'bootcamp_id')
+    }
+  };
